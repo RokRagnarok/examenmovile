d5d979cdf5fcacaf71ff24333ad5349ebfea76df
Rok Ragnarok <yoekzumaya@gmail.com>
2022-05-20 11:17:47 -0500

    main

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './HomeScreen';
+import NewScreen from './NewScreen';
+import SettingsScreen from './SettingsScreen';
+import React from 'react';
+import {createDrawerNavigator } from '@react-navigation/native/drawer';
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+ <Tab.Navigator>
+   <Tab.Screen name="Home" component={HomeScreen}/>
+   <Tab.Screen name="New" component={NewScreen}/>
+   <Tab.Screen name="Settings" component={SettingsScreen}/>
+ </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+function HomeScreen(props) {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Hola</Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
